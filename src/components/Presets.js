1661a036e204c51f86486c6900dfa125bef9c85c
Anthony <chitowncoder@gmail.com>
2020-10-15 22:56:00 -0500

    I found an error and repaired it on the Glider, working as it should

--- src/components/Presets.js
+++ src/components/Presets.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import ButtonToolbar from "react-bootstrap/ButtonToolbar";
-
 class Presets extends React.Component {
   render() {
     return (
       <div className="center">
-        <h4>Selection</h4>
-        <Button variant="outline-light" size="sm" onClick={this.props.glider}>
-          Glider
-        </Button>
-        <Button variant="outline-light" size="sm" onClick={this.props.pentaDecathlon}>
-          Pentadecathlon
-        </Button>
+        <h4>Presets</h4>
+        <ButtonToolbar>
+          <Button variant="outline-light" size="sm" onClick={this.props.glider}>
+            Glider
+          </Button>
+        </ButtonToolbar>
       </div>
-    )
+    );
   }
 }
 
